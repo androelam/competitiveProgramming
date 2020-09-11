aacb90cfddf5157c39ac2cf8f20f56088f1e23f7
Elambaruthi <androelam@gmail.com>
2020-09-11 07:58:13 +0530

    Adding ReverseString

---
+++ Easy/String/ReverseString.java
@@ -0,0 +1,14 @@
+class ReverseStringz {
+    public void reverseString(char[] s) {
+        int N = s.length-1;
+        int i = 0;
+        while(i < N) {
+            s[i] ^= s[N];
+            s[N] ^= s[i];
+            s[i] ^= s[N];
+            i++;
+            N--;
+        }
+    }
+}
+//Reverse string without extra space
