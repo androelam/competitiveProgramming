43974747b637ac58e4039d9fb1851d0eeccb0529
Elambaruthi <androelam@gmail.com>
2020-09-14 00:12:42 +0530

    RotateArray by k steps

---
+++ Easy/Array/RotateArray.java
@@ -0,0 +1,19 @@
+class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int numLen = nums.length;
+        k = k % numLen;
+        rotateArray(nums, 0, numLen - 1);
+        rotateArray(nums, 0, k - 1);
+        rotateArray(nums, k, numLen - 1);
+    }
+    public void rotateArray(int[] nums, int start, int end) {
+        
+        while (start < end) {
+            nums[start] ^= nums[end];
+            nums[end] ^= nums[start];
+            nums[start] ^= nums[end];
+            start++;
+            end--;
+        }
+    }
+}
