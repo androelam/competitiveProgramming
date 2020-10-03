b0fe4beacbe791e96a8a2b1722ec4465359dc11d
Elambaruthi <androelam@gmail.com>
2020-10-03 23:41:26 +0530

    LongestSubstringwithoutRepeatingCharacter

---
+++ Medium/LongestSubstring.java
@@ -0,0 +1,22 @@
+public class LongestSubStringWithoutRepeatingCharacter {  
+        public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int ptr1 = 0;
+        int ptr2 = 0;
+        HashSet<Character> hSet = new HashSet();
+        
+            
+           while(ptr2 < s.length()){
+            if (!hSet.contains(s.charAt(ptr2))) {
+               hSet.add(s.charAt(ptr2));
+               ptr2++;
+               max = Math.max(max, hSet.size());
+           }
+            else{
+                hSet.remove(s.charAt(ptr1));
+                ptr1++;
+            } 
+           }
+          return max;
+    }
+}
