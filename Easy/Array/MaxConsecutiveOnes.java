c5ce54a7ab9340bde4bd03aaa3a0bc0b3a019116
Elambaruthi <androelam@gmail.com>
2020-10-11 23:33:02 +0530

    another solution

--- Easy/Array/MaxConsecutiveOnes.java
+++ Easy/Array/MaxConsecutiveOnes.java
@@ -15,3 +15,22 @@ class MaxConsecutiveOnes {
         }
     
 }
+
+// class Solution {
+//     public int findMaxConsecutiveOnes(int[] nums) {
+//         int max = 0;
+//         int count = 0;
+//          for( int num : nums){
+//               if(num == 0) {
+//                  if(count>max) {
+//                      max=count;
+//                  }
+//                   count=0;
+//               }
+//                else if( num== 1) {                  
+//                      count++;
+//                }
+//          }
+//         return Math.max(count,max);
+//     }
+// }
