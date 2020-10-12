0c1334a5230c82436909a6ed345ffd26e0f2824b
Elambaruthi <androelam@gmail.com>
2020-10-12 22:59:57 +0530

    Is power of 2

---
+++ bit/PowerofTwo.java
@@ -0,0 +1,8 @@
+class PowerofTwo {
+    bool isPowerOfTwo(int x) 
+    { 
+         // First x in the below expression is 
+         // for  the case when x is 0  
+         return x && (!(x & (x - 1))); 
+    }   
+}
