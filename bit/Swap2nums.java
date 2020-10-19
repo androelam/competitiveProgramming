47024a13bccd4a4ce0d8fcf1730018bb1bf59fbf
Elambaruthi <androelam@gmail.com>
2020-10-19 21:59:41 +0530

    Swap 2 nums

---
+++ bit/Swap2nums.java
@@ -0,0 +1,12 @@
+public class Swap2nums {
+    public static void main(String a[])
+    {
+        int x = 10;
+        int y = 5;
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("After swaping:"
+                           + " x = " + x + ", y = " + y);
+    }
+}
